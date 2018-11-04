4ee26804d9d2f267c241b6e65792f928ced041d8
SamMendes <samuelmendesbrasil@gmail.com>
2018-11-04 01:35:48 -0200

    Testes criados para os controllers da API

---
+++ PizzariaApp.Tests/Controllers/PedidosControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PizzariaApp.Models.Enum;
+using PizzariaWebApi.Controllers;
+using PizzariaWebApi.Models;
+
+namespace PrjPizzaria.Tests.Controllers
+{
+    [TestClass]
+    public class PedidosControllerTest
+    {
+        [TestMethod]
+        public void Post()
+        {
+            PedidosController pedidosController = new PedidosController();
+            bool resultado = pedidosController.Post((PizzaTamanhoEnum)2, (PizzaSaborEnum)1);
+            Assert.IsTrue(resultado);
+        }
+
+        [TestMethod]
+        public void Get()
+        {
+            PedidosController pedidosController = new PedidosController();
+            PedidoModel pedidoEncontrado = pedidosController.Get(1);
+
+            Assert.IsNotNull(pedidoEncontrado);
+            Assert.AreNotEqual(pedidoEncontrado.Id, 0);
+            Assert.AreEqual(PizzaTamanhoEnum.media, pedidoEncontrado.Pizza.Tamanho.Tamanho);
+            Assert.AreEqual(PizzaSaborEnum.calabresa, pedidoEncontrado.Pizza.Sabor.Sabor);
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            PedidosController pedidosController = new PedidosController();
+            bool resultado = pedidosController.Put(1, (PizzaPersonalizacaoEnum)1);
+
+            Assert.IsTrue(resultado);
+            PedidoModel pedidoEncontrado = pedidosController.Get(1);
+
+            Assert.IsNotNull(pedidoEncontrado);
+            Assert.AreNotEqual(pedidoEncontrado.Id, 0);
+            Assert.AreEqual(PizzaTamanhoEnum.media, pedidoEncontrado.Pizza.Tamanho.Tamanho);
+            Assert.AreEqual(PizzaSaborEnum.calabresa, pedidoEncontrado.Pizza.Sabor.Sabor);
+            Assert.AreEqual(PizzaPersonalizacaoEnum.extraBacon, pedidoEncontrado.Pizza.Personalizacoes[0].Personalizacao);
+        }
+
+        [TestMethod]
+        public void GetResumoPedido()
+        {
+            PedidosController pedidosController = new PedidosController();
+            PedidoResumoModel resumoPedido = pedidosController.GetResumoPedido(1);
+
+            Assert.IsNotNull(resumoPedido);
+            Assert.AreEqual(resumoPedido.SaborPizza, "Calabresa");
+            Assert.AreEqual(resumoPedido.TamanhoPizza, "Média");
+            Assert.AreEqual(resumoPedido.PrecoPizza, 30.0m);
+            Assert.AreEqual(resumoPedido.PersonalizacoesPizza[0].Personalizacao, "Extra Bacon");
+            Assert.AreEqual(resumoPedido.PersonalizacoesPizza[0].Preco, 3.0m);
+            Assert.AreEqual(resumoPedido.TotalPedido, 33.0m);
+            Assert.AreEqual(resumoPedido.TempoPreparo, "20 minutos");
+        }
+    }
+}
---
+++ PizzariaApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As Informações Gerais sobre um assembly são controladas por meio do 
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("PizzariaApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PizzariaApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Configurar o ComVisible como false torna os tipos desse assembly invisíveis 
+// para componentes COM.  Se for necessário acessar um tipo nesse assembly a partir do 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("39a6423d-a202-455b-9a91-1ba8357375e2")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou utilizar como padrão os Números da Versão e de Revisão 
+// usando o '*' como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
